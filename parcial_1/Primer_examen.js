8cbf2a5a32920c686d6ea95257fd516465cf76f1
JesusG0nzalez <alexvelazquez1228@gmail.com>
2024-02-02 17:42:05 -0800

    first_test

--- parcial_1/Primer_examen.js
+++ parcial_1/Primer_examen.js
@@ -1,6 +1,70 @@
 var url = "https://jsonplaceholder.typicode.com/todos";
 
 fetch(url).then(response => response.json()).then(data => {
+    var lista = [];
+
     data.forEach(element => {
-        console.log("ID: " + element.id);
-    }) } )
+        lista.push(element.id);
+    });
+
+    //Lista de todos los pendientes solo IDS
+    console.log("\nLISTA DE TODOS LOS PENDIENTES (Solo IDS): \n"+ lista);
+
+})
+
+fetch(url).then(response => response.json()).then(data => {
+    var lista_pendientes = [];
+    var lista_resuelto = [];
+    var lista_sin_resuelto = [];
+
+    data.forEach(element => {
+        lista_pendientes.push("IDS: "+ element.id +"Title: " +element.title);
+
+        if (element.completed == true){
+            lista_resuelto.push("IDS: "+ element.id +"Title: " +element.title);
+        }
+        else
+        {
+            lista_sin_resuelto.push("IDS: "+ element.id +"Title: " +element.title);
+        }
+    });
+
+    //lista de pendientes solo IDS y TITULOS
+    console.log("\nLISTA DE TODOS LOS PENDIENTES (IDS y titles): \n "+ lista_pendientes);
+    
+    //lista de pendientes sin resolver solo IDS y TITULOS
+    console.log("\nLISTA DE TODOS LOS PENDIENTES SIN RESOLVER: (IDS y titles) \n"+ lista_sin_resuelto);
+    
+    //lista de pendientes resueltos solo IDS y TITULOS
+    console.log("\nLISTA DE TODOS LOS PENDIENTES RESUELTOS: (IDS y titles) \n"+ lista_resuelto);
+
+
+})
+
+fetch(url).then(response => response.json()).then(data => {
+    var lista_pendientes = [];
+    var lista_resuelto = [];
+    var lista_sin_resuelto = [];
+
+    data.forEach(element => {
+        lista_pendientes.push("IDS: "+ element.id +"userId: " +element.userId);
+
+        if (element.completed == true){
+            lista_resuelto.push("IDS: "+ element.id +"userId: " +element.userId);
+        }
+        else
+        {
+            lista_sin_resuelto.push("IDS: "+ element.id +"userId: " +element.userId);
+        }
+    });
+
+    //lista de pendientes solo IDS y USERID
+    console.log("\nLISTA DE TODOS LOS PENDIENTES: (IDS y UserID) \n"+ lista_pendientes);
+
+    //lista de pendientes sin resolver solo IDS y USERID
+    console.log("\nLISTA DE TODOS LOS PENDIENTES SIN RESOLVER: (IDS y UserID) \n"+ lista_sin_resuelto);
+
+    //lista de pendientes resueltos solo IDS y USERID
+    console.log("\nLISTA DE TODOS LOS PENDIENTES RESUELTOS: (IDS y UserID) \n"+ lista_resuelto);
+
+})
